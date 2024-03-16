4fc4232ded23c8cdc264ef93d2ac935b6ba1e462
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-03-16 11:39:30 -0700

    struggle bussing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    const options = { credentials: "include" };
+    fetch("http://localhost:8000/api/auth", options)
+      .then(async (response) => await response.json())
+      .then((json) => console.log(json));
+  }, []);
+
   return (
     <>
       <h1>Hello</h1>
