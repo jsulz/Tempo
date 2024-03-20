cdb423ab9b1af4ef9f9be68d014983a317412da9
jsulz <j.sulzdorf@gmail.com>
2024-03-20 13:36:16 -0700

    trying to debug production issue

--- utils/spotifyclient.ts
+++ utils/spotifyclient.ts
@@ -129,7 +129,11 @@ export async function refreshAccessTokens(tokens: Tokens): Promise<Tokens> {
   const body = await fetch(url, payload);
   const response = await body.json();
 
-  console.log(body.headers);
+  console.log("body");
+  console.log(body);
+
+  console.log("response");
+  console.log(response);
 
   // Set up the tokens that we will store for future API calls
   const currentTime = new Date().getTime() / 1000;
