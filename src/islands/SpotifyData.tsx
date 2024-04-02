123a6d646750bcbb28c554d3a4b61a802c237f0f
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-04-02 09:02:42 -0700

    refactoring tracks into their own component

--- src/islands/SpotifyData.tsx
+++ src/islands/SpotifyData.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { TrackObj } from "../../utils/types.ts";
 import Heading from "../components/Heading.tsx";
+import Tracks from "./Tracks.tsx";
 
 /**
  * Renders Spotify data from the provided data object prop.
@@ -41,55 +42,16 @@ export default function SpotifyData({
         );
         html.push(heading);
         if (property == "tracks") {
-          const final = (
-            <div
-              className="row row-cols-2 row-cols-sm-4 row-cols-lg-5"
+          const tracks = (
+            <Tracks
+              tracks={spotifyCollection[property]}
+              playing={playing}
+              currentlyPlaying={currently_playing}
+              handleTrackPlaying={handleTrackClick}
               key={`${property.toUpperCase()}-items`}
-            >
-              {spotifyCollection[property].map((item) => {
-                let icon = null;
-                const play = <i className="bi bi-play-circle"></i>;
-                const pause = <i className="bi bi-pause-circle"></i>;
-                if (item.uri == currently_playing.uri) {
-                  icon = playing ? pause : play;
-                } else {
-                  icon = play;
-                }
-
-                return (
-                  <div className="col" key={item.id}>
-                    <img className="img-fluid" src={item.album.images[1].url} />
-                    <p>
-                      <a href={item.external_urls.spotify}>{item.name}</a> from{" "}
-                      <a href={item.album.external_urls.spotify}>
-                        {item.album.name}
-                      </a>
-                    </p>
-                    <p>
-                      By:{" "}
-                      {item.artists.map((artist) => {
-                        return (
-                          <>
-                            <a href={artist.external_urls.spotify}>
-                              {artist.name}
-                            </a>
-                            &nbsp;
-                          </>
-                        );
-                      })}
-                    </p>
-                    <button
-                      className="btn btn-primary"
-                      onClick={(e) => handleTrackClick(e, item)}
-                    >
-                      {icon}
-                    </button>
-                  </div>
-                );
-              })}
-            </div>
+            />
           );
-          html.push(final);
+          html.push(tracks);
         } else {
           const column_els = spotifyCollection[property].map((item) => {
             return (
